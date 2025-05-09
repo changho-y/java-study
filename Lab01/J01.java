f3aea4731acb90bace2e10feba89b8cc868eb1b2
changho-y <changho8838@naver.com>
2025-05-10 06:06:58 +0900

    Create J01.java
    J-01 소스코드

---
+++ Lab01/J01.java
@@ -0,0 +1,7 @@
+// J-01
+public class J01 {								// J01 이름의 클래스 선언
+	public static void main(String[] args) {	// 자바 프로그램의 실행 시작 메소드(함수)
+		int n = 20202156;						// 정수형 변수 n을 선언하고 20202156 정수값으로 초기화
+		System.out.println("윤창호" + n);			// "윤창호"+n의 결과로 "윤창호20202156"을 출력
+	}
+}
