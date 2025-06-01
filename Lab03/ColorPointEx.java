64d4bcaaf8cdb9e747a43fc030e0f3edbe291ac7
changho-y <changho8838@naver.com>
2025-06-01 19:21:53 +0900

    u

---
+++ Lab03/ColorPointEx.java
@@ -0,0 +1,34 @@
+// J-23		클래스 상속 만들기
+class Point {
+	private int x, y; // 한 점을 구성하는 x, y 좌표
+	public void set(int x, int y) {
+		this.x = x; this.y = y;
+	}
+	public void showPoint() {
+		System.out.println("(" + x + "," + y + ")");
+	}
+}
+
+class ColorPoint extends Point { // Point 를 상속받은 ColorPoint 선언
+	private String color; // 점의 색
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void showColorPoint() { //컬러 점의 좌표 출력
+		System.out.println(color);
+		showPoint(); // Point 클래스의 showPoint() 호출
+	}
+}
+
+public class ColorPointEx {
+	public static void main(String [] agrs) {
+		Point p = new Point(); // Point 객체 생성
+		p.set(1, 2); // Point 클래스의 set() 호출
+		p.showPoint();
+		
+		ColorPoint cp = new ColorPoint(); // ColorPoint 객체 생성
+		cp.set(3, 4); // Point 클래스의 set() 호출
+		cp.setColor("red"); // ColorPoint 클래스의 setColor() 호출
+		cp.showColorPoint(); // 컬러와 좌표 출력
+	}
+}
