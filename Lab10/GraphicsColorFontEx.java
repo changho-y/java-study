69f00e5c823c1eab321fb42563b6cf64e47a253f
changho-y <83066788+changho-y@users.noreply.github.com>
2025-08-19 22:09:23 +0900

    create

---
+++ Lab10/GraphicsColorFontEx.java
@@ -0,0 +1,35 @@
+// J-99
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsColorFontEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+	
+	public GraphicsColorFontEx() {
+		setTitle("Color,Font Ex");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(350,470);
+		setVisible(true);
+	}
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.BLUE);
+			g.drawString("Good Java", 30, 30);
+			g.setColor(new Color(255,0,0));
+			g.setFont(new Font("Arial", Font.ITALIC, 30));
+			g.drawString("King Java", 30, 60);
+			g.setColor(new Color(0x00ff00ff));
+			for(int i=1; i<5; i++) {
+				g.setFont(new Font("Arial", Font.ITALIC, i*10));
+				g.drawString("gooooooooood", 30, 60+i*60);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		new GraphicsColorFontEx();
+	}
+
+}
